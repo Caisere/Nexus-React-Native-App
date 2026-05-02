765986e4510d5436bbdda4e963d50b5b50e476fb
Omoshola <heylehgbedey@gmail.com>
2026-05-02 13:25:55 -0700

    styling: replace welcome screen styling with NativeWind utility stying

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,33 +1,24 @@
-import "@/global.css";
 import { Colors } from "@/constants/colors";
+import "@/global.css";
 import { useAuthStore } from "@/store/authStore";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { ActivityIndicator, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
-
-
 export default function RootLayout() {
-  const isHydrated = useAuthStore((state) => state.isHydrated);
+  const { isHydrated } = useAuthStore();
 
   if (!isHydrated) {
     return (
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: Colors.background,
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <ActivityIndicator color={Colors.primary} size="large" />
+      <View className="flex-1 bg-background items-center justify-center">
+        <ActivityIndicator className="text-primary" size="large" />
       </View>
     );
   }
 
   return (
-    <SafeAreaProvider style={{ backgroundColor: Colors.background }}>
+    <SafeAreaProvider className="bg-background">
       <StatusBar style="light" backgroundColor={Colors.background} />
       <Stack
         screenOptions={{
